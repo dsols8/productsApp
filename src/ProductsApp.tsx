d61ef0effa41574a8cf283038db62c340ed8dd9a
Darío Solís <dariosolisjara@gmail.com>
2024-10-28 23:39:14 -0600

    auth ready, now need to mske the register myself

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -9,14 +9,15 @@ import * as eva from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import { useColorScheme } from 'react-native';
+import { AuthProvider } from './presentation/providers/AuthProvider';
 
 export const ProductsApp = () => {
 
     const colorScheme = useColorScheme();
     const theme = colorScheme === 'dark' ? eva.dark : eva.light;
     const backgroundColor = (colorScheme === 'dark')
-    ? theme['color-basic-800']
-    : theme['color-basic-100']
+        ? theme['color-basic-800']
+        : theme['color-basic-100']
 
     return (
         <>
@@ -25,19 +26,22 @@ export const ProductsApp = () => {
                 {...eva} theme={theme}
             >
                 <NavigationContainer
-                theme = {{
-                    dark: colorScheme === 'dark',
-                    colors: {
-                        primary: theme['color-primary-500'],
-                        background: backgroundColor,
-                        card: theme['color-basic-100'],
-                        text: theme['text-basic-color'],
-                        border: theme['color-basic-800'],
-                        notification: theme['color-primary-500'],
-                    }
-                }}
+                    theme={{
+                        dark: colorScheme === 'dark',
+                        colors: {
+                            primary: theme['color-primary-500'],
+                            background: backgroundColor,
+                            card: theme['color-basic-100'],
+                            text: theme['text-basic-color'],
+                            border: theme['color-basic-800'],
+                            notification: theme['color-primary-500'],
+                        }
+                    }}
                 >
-                    <StackNavigator />
+                    <AuthProvider>
+                        <StackNavigator />
+                    </AuthProvider>
+
                 </NavigationContainer>
             </ApplicationProvider>
         </>
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,14 +1,19 @@
 import { Button, Icon, Layout, Text } from '@ui-kitten/components'
+import { useAuthStore } from '../../store/auth/useAuthStore'
 
 export const HomeScreen = () => {
+
+    const {logout} = useAuthStore();
+
     return (
         <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>HomeScreen</Text>
 
             <Button
-            accessoryLeft={<Icon name='lock'/>}
+            accessoryRight={<Icon name='log-out-outline'/>}
+            onPress={logout}
             >
-                unlock my pussy
+                Log Out
             </Button>
         </Layout>
     )
